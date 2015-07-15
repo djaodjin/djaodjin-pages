422a4fd85412aeb51a2c0095c34b43f3603a404b
Sebastien Mirolo <smirolo@djaodjin.com>
2015-07-15 16:35:14 -0700

    security fix for upload theme

--- pages/api/upload_template.py
+++ pages/api/upload_template.py
@@ -23,166 +23,66 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import os
-import zipfile, tempfile, shutil
+import os, zipfile
 
+from django.conf import settings as django_settings
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.db.models import Q
-from django.utils import timezone
-
+from django.utils._os import safe_join
 from rest_framework import status, generics
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 
-from pages.models import UploadedTemplate
-from pages.serializers import UploadedTemplateSerializer
-from pages.settings import (
-    UPLOADED_TEMPLATE_DIR,
-    UPLOADED_STATIC_DIR,
-    DISABLE_ACCOUNT_TEMPLATE_PATH)
+from ..mixins import AccountMixin
+from ..models import UploadedTemplate
+from ..serializers import UploadedTemplateSerializer
+from ..themes import install_theme
 
-from pages.mixins import AccountMixin
 
-class UploadedTemplateListAPIView(AccountMixin, generics.ListCreateAPIView):
-    serializer_class = UploadedTemplateSerializer
-    parser_classes = (FileUploadParser,)
+class UploadedTemplateMixin(AccountMixin):
 
     def get_queryset(self):
         queryset = UploadedTemplate.objects.filter(
             Q(account=self.get_account())|Q(account=None))
         return queryset
 
-    def post(self, request, format=None, *args, **kwargs):
-        #pylint: disable=unused-argument,redefined-builtin
-        #pylint: disable=too-many-locals,too-many-statements
-        new_package = True
-        account = self.get_account()
-        if account and not DISABLE_ACCOUNT_TEMPLATE_PATH:
-            uploaded_template_dir = os.path.join(
-                UPLOADED_TEMPLATE_DIR, account.slug)
-            uploaded_static_dir = os.path.join(
-                UPLOADED_STATIC_DIR, account.slug)
-        else:
-            uploaded_template_dir = UPLOADED_TEMPLATE_DIR
-            uploaded_static_dir = UPLOADED_STATIC_DIR
-        file_obj = request.FILES['file']
-        # zfile = zipfile.ZipFile(file_obj)
 
-        if zipfile.is_zipfile(file_obj):
-            zfile = zipfile.ZipFile(file_obj)
-        else:
-            return Response({'info': "Invalid zipfile"},
-                status=status.HTTP_400_BAD_REQUEST)
+class UploadedTemplateListAPIView(UploadedTemplateMixin,
+                                  generics.ListCreateAPIView):
 
-        root_path = str(file_obj).replace('.zip', '')
-        root_path_templates = str(file_obj).replace('.zip', '/templates/')
-        root_path_static = str(file_obj).replace('.zip', '/static/')
-
-        if os.path.exists(
-            os.path.join(uploaded_template_dir, root_path)):
-            new_package = False
-
-        dir_temp = os.path.join(tempfile.gettempdir(), root_path)
-        if not os.path.exists(dir_temp):
-            os.makedirs(dir_temp)
-
-        temp_dir_templates = os.path.join(dir_temp, 'templates')
-        if not os.path.exists(temp_dir_templates):
-            os.makedirs(temp_dir_templates)
-
-        temp_dir_static = os.path.join(dir_temp, 'static')
-        if not os.path.exists(temp_dir_static):
-            os.makedirs(temp_dir_static)
-
-        members = zfile.namelist()
-        templates_to_extract = [m for m in members\
-            if m.startswith(root_path_templates) and m != root_path_templates]
-        static_to_extract = [m for m in members\
-            if m.startswith(root_path_static) and m != root_path_static]
-
-        for name in zfile.namelist():
-            # remove __MACOSX File and DS_Store
-            if name.startswith('/'):
-                shutil.rmtree(dir_temp)
-                return Response("Invalid zipfile",
-                    status=status.HTTP_400_BAD_REQUEST)
-
-            if not "__MACOSX" in name and not ".DS_Store" in name:
-                if name in templates_to_extract or name in static_to_extract:
-                    if name in templates_to_extract:
-                        is_template = True
-                        directory = temp_dir_templates
-                    elif name in static_to_extract:
-                        is_template = False
-                        directory = temp_dir_static
-
-                    if is_template:
-                        new_name = name.replace('%s/templates/' % root_path, '')
-                    else:
-                        new_name = name.replace('%s/static/' % root_path, '')
-
-                    if str(name).endswith('/'):
-                        os.makedirs(os.path.join(directory, new_name))
-                    else:
-                        if is_template:
-                            if not name.endswith(('.html', '.jinja2')):
-                                shutil.rmtree(dir_temp)
-                                return Response(
-                                    "Templates directory has to \
-                                    contain only html or jinja2 file",
-                                    status=status.HTTP_403_FORBIDDEN)
-                        else:
-                            if not name.endswith('.css') \
-                                and not name.endswith('.js')\
-                                and not name.endswith('.css.map')\
-                                and not name.endswith('.png'):
-                                shutil.rmtree(dir_temp)
-                                return Response(
-                                    "Static directory has to \
-                                    contains only css or js file",
-                                    status=status.HTTP_403_FORBIDDEN)
-                        outfile = open(os.path.join(directory, new_name), 'wb')
-                        outfile.write(zfile.read(name))
-                        outfile.close()
-
-        if os.path.exists(
-            os.path.join(uploaded_template_dir, root_path)):
-            shutil.rmtree(
-                os.path.join(uploaded_template_dir, root_path))
-
-        shutil.move(
-            temp_dir_templates,
-            os.path.join(uploaded_template_dir, root_path))
-
-        if os.path.exists(
-            os.path.join(uploaded_static_dir, root_path)):
-
-            shutil.rmtree(
-                os.path.join(uploaded_static_dir, root_path))
-
-        if os.listdir(temp_dir_static):
-            shutil.move(
-                temp_dir_static,
-                os.path.join(uploaded_static_dir, root_path))
-
-        # delete temp directory
-        shutil.rmtree(dir_temp)
-
-        if new_package:
-            template_package = UploadedTemplate(account=account,
-                name=root_path.replace('/', ''))
-            template_package.updated_at = timezone.now()
-            template_package.save()
-        else:
-            template_package = UploadedTemplate.objects.get(
-                account=account,
-                name=root_path.replace('/', ''))
-            template_package.updated_at = timezone.now()
-            template_package.save()
-        serializer = UploadedTemplateSerializer(template_package)
-        return Response(serializer.data, status=200)
-
-
-class UploadedTemplateAPIView(AccountMixin, generics.RetrieveUpdateAPIView):
+    parser_classes = (FileUploadParser,)
     serializer_class = UploadedTemplateSerializer
-    model = UploadedTemplate
 
+    def post(self, request, *args, **kwargs):
+        file_obj = request.data['file']
+        theme_name = os.path.splitext(os.path.basename(file_obj.name))[0]
+        templates_dir = safe_join(django_settings.TEMPLATE_DIRS[0], theme_name)
+        if os.path.exists(templates_dir):
+            # If we do not have an instance at this point, the directory
+            # might still exist and belong to someone else when pages
+            # tables are split amongst multiple databases.
+            raise PermissionDenied()
+        if zipfile.is_zipfile(file_obj):
+            with zipfile.ZipFile(file_obj) as zip_file:
+                install_theme(theme_name, zip_file)
+            UploadedTemplate.objects.create(
+                name=theme_name, account=self.get_account())
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'info': "Invalid archive"},
+            status=status.HTTP_400_BAD_REQUEST)
+
+
+class UploadedTemplateAPIView(UploadedTemplateMixin,
+                              generics.RetrieveUpdateAPIView):
+
+    serializer_class = UploadedTemplateSerializer
+    slug_url_kwarg = 'theme'
+
+    def get_object(self):
+        try:
+            return self.get_queryset().get(
+                name=self.kwargs.get(self.slug_url_kwarg))
+        except UploadedTemplate.DoesNotExist:
+            raise Http404("theme %s not found"
+                % self.kwargs.get(self.slug_url_kwarg))
