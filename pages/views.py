bb8bd55fcb0fa49f9345bc52513dacbfc5ac5677
Stephane Robino <stephane.robino@gmail.com>
2015-12-22 10:32:46 +0100

    Clean pylint report

--- pages/views.py
+++ pages/views.py
@@ -21,7 +21,10 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-import markdown, os, tempfile, zipfile, shutil
+
+#pylint:disable=unused-argument
+
+import markdown, os, zipfile
 
 from StringIO import StringIO
 from bs4 import BeautifulSoup
